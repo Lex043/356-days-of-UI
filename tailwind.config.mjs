24ed6a2fac05593eb4640f031dafc1ffcff55ea2
Oyebamiji Alex <alexferanmi390@gmail.com>
2024-11-21 12:12:31 +0100

    feat: Day 2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            boxShadow: {
+                "inner-custom":
+                    "inset 4px 4px 6px #7F7F7F, inset -4px -4px 6px #FFFFFF",
+            },
+        },
+    },
+    plugins: [],
+};
